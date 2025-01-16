242cdd197db8cfd75002ea146aed18cdbff6e1cf
luigitheartist <barbosaj@roverkids.org>
2025-01-16 13:42:49 -0500

    4.3

--- Prototype 4/Assets/Scripts/PlayerControl.cs
+++ Prototype 4/Assets/Scripts/PlayerControl.cs
@@ -21,24 +21,27 @@ public class PlayerControl : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(fp.transform.forward * speed * forwardInput);
-        powerupIndicator.transform.position=transform.position+new Vector3(0,-0.5f,0);
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -.6f, 0);
+    
     }
 
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Powerup"))
+        if (other.CompareTag("powup"))
         {
             hasPowerup = true;
+            powerupIndicator.gameObject.SetActive(true);
             Destroy(other.gameObject);
             StartCoroutine(PowerupCountdownRoutine());
-            powerupIndicator.gameObject.SetActive(true);
+            
         }
     }
    IEnumerator PowerupCountdownRoutine()
     {
         yield return new WaitForSeconds(7);
         hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -50,7 +53,7 @@ public class PlayerControl : MonoBehaviour
             Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
             Debug.Log("Collided with " + collision.gameObject.name + " with powerup set to " + hasPowerup);
             enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
-            powerupIndicator.gameObject.SetActive(false);
+           
         }
 
     }
